13c37349d57dff1e21c284085764660cdf95684c
ghinafitri04 <ghinafitri605@gmail.cpm>
2024-01-21 01:44:52 +0700

    Controller Untuk ke dashboard

---
+++ app/Http/Controllers/AdminProvinsiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminProvinsiController extends Controller
+{
+    public function showDashboard()
+    {
+        return view('admin_provinsi_dashboard');
+    }
+}
