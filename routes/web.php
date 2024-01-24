63e8f6ad600b49b6704a617155a6d4bdf32e21cf
Muhammad Irsyadul Fikri <irsyadulfikri270418@gmail.com>
2024-01-24 15:44:12 +0700

    route dikit

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 use App\Http\Controllers\HalamanController;
