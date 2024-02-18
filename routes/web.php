d724244c75c4b548ece2178e44926bf3626adceb
Muhammad Irsyadul Fikri <irsyadulfikri270418@gmail.com>
2024-02-18 09:32:38 +0700

    okey

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::middleware(['auth:admin_provinsi'])->group(function () {
     Route::put('/admin-provinsi/manajemen-dps/{id}', [AdminProvinsiManajemenDpsController::class, 'update'])
         ->name('admin_provinsi.manajemen_dps.update');
     Route::get('/sertifikat/{filename}', function ($filename) {
-        $path = storage_path('app/public/sertifikat/' . $filename);
+        $path = storage_path('app/sertifikat/' . $filename);
 
         if (!File::exists($path)) {
             abort(404);
