f60c32f49063ae1cd7bda1fba1138b34ead95a42
ghinafitri04 <ghinafitri605@gmail.cpm>
2024-03-16 11:19:09 +0700

    update dikit

--- resources/views/koperasi_proses_konversi.blade.php
+++ resources/views/koperasi_proses_konversi.blade.php
@@ -11,6 +11,18 @@
     <link rel="stylesheet" href="{{ asset('css/proses_konversi1.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        /* Custom CSS styles */
+        .footer-buttons {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        #pdf-preview-link {
+            margin-top: 85px;
+        }
+    </style>
 </head>
 
 <body>
@@ -80,10 +92,11 @@
                             <form id="formUpload" action="{{ route('prosesTahap1Submit') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <label for="rapat_anggota" class="custom-file-upload">
-                                    <input type="file" id="rapat_anggota" name="rapat_anggota" style="display:none">
+                                    <input type="file" id="rapat_anggota" name="rapat_anggota" style="display:none" onchange="updateFileName()">
                                     <img src="/img/uploadfile.png" alt="Your Image" id="pdf-preview-img" class="centered-image">
-                                    <p class="image-caption">Upload Dokumen Rapat Anggota</p>
+                                    <span id="file-selected-text" class="image-caption">Upload Dokumen Rapat Anggota</span>
                                 </label>
+                                
 
                                 <!-- Footer buttons with reversed order -->
                                 <div class="footer-buttons text-right mt-3">
@@ -91,7 +104,7 @@
                                         <img src="/img/selanjutnya.png" alt="Next Icon" />
                                         Selanjutnya
                                     </a>
-                                    <button type="button" class="footer-button footer-button-left" id="simpanButton">
+                                    <button type="submit" class="footer-button footer-button-left" id="simpanButton">
                                         <img src="/img/simpanfile.png" alt="Save Icon" />
                                         Simpan
                                     </button>
@@ -105,10 +118,14 @@
                             </form>
                         </div>
 
-                        <!-- Preview PDF link -->
+                                                <!-- Preview PDF link -->
                         <div id="pdf-preview-link" style="display: none;">
-                            <a id="preview-link" target="_blank" href="#">Lihat Pratinjau PDF</a>
+                            <span id="preview-text">Lihat Kesepakatan Anggota</span>
                         </div>
+
+
+                        <!-- PDF Preview Frame -->
+                        <iframe id="pdf-preview-frame" style="width:100%; height:500px; display:none;"></iframe>
                     </div>
                 </div>
             </div>
@@ -116,95 +133,99 @@
     </div>
 
     <!-- jQuery and Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
     <script>
-        document.getElementById('simpanButton').addEventListener('click', function () {
-            var fileName = document.getElementById('rapat_anggota').value.split('\\').pop();
-            var confirmUpload = confirm("Apakah Anda yakin akan mengupload " + fileName + "?");
-            if (confirmUpload) {
-                document.getElementById('formUpload').submit();
-            } else {
-                // Batalkan pengiriman formulir
+        // Panggil fungsi setInitialStep saat halaman dimuat
+        window.onload = function () {
+            // Pindahkan pemanggilan setInitialStep() ke dalam bagian ini
+            setInitialStep();
+    
+            // Cek apakah ada file yang diunggah pada sesi sebelumnya
+            const uploadedFile = localStorage.getItem('uploadedFile');
+            if (uploadedFile) {
+                // Jika ada, perbarui pratinjau dengan file tersebut
+                document.getElementById('pdf-preview-frame').src = uploadedFile;
+                // Tampilkan iframe dengan pratinjau PDF
+                document.getElementById('pdf-preview-frame').style.display = 'block';
+                // Tampilkan link preview PDF
+                document.getElementById('pdf-preview-link').style.display = 'block';
+                document.getElementById('preview-link').href = uploadedFile;
+            }
+        };
+    
+        // Panggil fungsi updatePdfPreview saat dokumen diunggah
+        document.getElementById('formUpload').addEventListener('submit', function (event) {
+            event.preventDefault(); // Hindari pengiriman form secara otomatis
+    
+            // Dapatkan file yang diunggah
+            const file = document.getElementById('rapat_anggota').files[0];
+    
+            if (file) {
+                var fileName = file.name;
+                var confirmUpload = confirm("Apakah Anda yakin akan mengupload " + fileName + "?");
+                if (confirmUpload) {
+                    // Panggil fungsi updatePdfPreview() setelah pengguna mengonfirmasi pengungahan file
+                    updatePdfPreview(file);
+                    // Simpan file yang diunggah di Local Storage
+                    const reader = new FileReader();
+                    reader.onload = function (e) {
+                        localStorage.setItem('uploadedFile', e.target.result);
+                    };
+                    reader.readAsDataURL(file);
+                } else {
+                    // Batalkan pengiriman formulir jika pengguna membatalkan pengungahan
+                }
             }
         });
+    
+        function updatePdfPreview(file) {
+            const pdfContainer = document.getElementById('pdf-preview-frame');
 
-        function updatePdfPreview() {
-            const input = document.getElementById('rapat_anggota');
-            const previewFrame = document.getElementById('pdf-preview-frame');
+            const fileReader = new FileReader();
+            fileReader.onload = function () {
+                const typedarray = new Uint8Array(this.result);
+                pdfjsLib.getDocument(typedarray).promise.then(function (pdf) {
+                    pdf.getPage(1).then(function (page) {
+                        const scale = 1.5;
+                        const viewport = page.getViewport({ scale: scale });
 
-            if (input.files && input.files[0]) {
-                const file = input.files[0];
-                const fileReader = new FileReader();
+                        const canvas = document.createElement('canvas');
+                        const context = canvas.getContext('2d');
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
 
-                fileReader.onload = function () {
-                    const typedarray = new Uint8Array(this.result);
-                    pdfjsLib.getDocument(typedarray).promise.then(function (pdf) {
-                        pdf.getPage(1).then(function (page) {
-                            const scale = 1.5;
-                            const viewport = page.getViewport({ scale: scale });
+                        const renderContext = {
+                            canvasContext: context,
+                            viewport: viewport
+                        };
 
-                            const canvas = document.createElement('canvas');
-                            const context = canvas.getContext('2d');
-                            canvas.height = viewport.height;
-                            canvas.width = viewport.width;
-
-                            const renderContext = {
-                                canvasContext: context,
-                                viewport: viewport
-                            };
-
-                            page.render(renderContext).promise.then(function () {
-                                // Hapus konten yang sudah ada sebelumnya pada iframe
-                                previewFrame.contentDocument.documentElement.innerHTML = '';
-                                previewFrame.src = canvas.toDataURL('image/png');
-                                // Tampilkan link preview PDF
-                                document.getElementById('pdf-preview-link').style.display = 'block';
-                                document.getElementById('preview-link').href = previewFrame.src;
-                            });
+                        page.render(renderContext).promise.then(function () {
+                            // Hapus konten yang sudah ada sebelumnya di dalam pdfContainer
+                            pdfContainer.innerHTML = '';
+                            // Tambahkan pratinjau PDF ke dalam pdfContainer
+                            pdfContainer.appendChild(canvas);
+                            // Tampilkan iframe dengan pratinjau PDF
+                            pdfContainer.style.display = 'block';
+                            // Tampilkan link preview PDF
+                            document.getElementById('pdf-preview-link').style.display = 'block';
+                            document.getElementById('preview-link').href = canvas.toDataURL('image/png');
                         });
                     });
-                };
-
-                fileReader.readAsArrayBuffer(file);
-            }
+                });
+            };
+            fileReader.readAsArrayBuffer(file);
         }
 
-        document.getElementById('rapat_anggota').addEventListener('change', function () {
-            var input = this;
-            var fileName = input.files[0].name;
-            var imageCaption = input.parentNode.querySelector('.image-caption');
-            imageCaption.textContent = fileName;
-        });
-
-        function updateStep(step) {
-            const circles = document.querySelectorAll('.step-circle');
-            const lines = document.querySelectorAll('.step-line');
-
-            circles.forEach((circle, index) => {
-                if (index < step) {
-                    circle.classList.add('active');
-                } else {
-                    circle.classList.remove('active');
-                }
-            });
-
-            lines.forEach((line, index) => {
-                line.style.backgroundColor = index < step - 1 ? '#00BCD4' : '#ccc';
-            });
-        }
-
-        // Fungsi untuk mengatur elemen yang sesuai saat halaman dimuat
         function setInitialStep() {
             // Hapus kelas active dari semua ikon pensil
             const pencils = document.querySelectorAll('.pencil-icon');
             pencils.forEach(pencil => {
                 pencil.classList.remove('active');
             });
-
+    
             // Tambahkan kelas active pada ikon pensil tahap 1
             const pencil1 = document.getElementById('pencil1');
             pencil1.classList.add('active');
@@ -212,27 +233,14 @@
             document.getElementById('pdf-preview-link').style.display = 'none';
         }
 
-        // Panggil fungsi setInitialStep saat halaman dimuat
-        window.onload = function () {
-            setInitialStep();
-            // updatePdfPreview(); // Tidak perlu memanggil di sini karena akan dipanggil saat mengunggah file
+        function updateFileName() {
+        const fileInput = document.getElementById('rapat_anggota');
+        const fileName = fileInput.files[0].name;
+        document.getElementById('file-selected-text').textContent = fileName;
+    }
 
-            // Cek apakah ada file yang diunggah pada sesi sebelumnya
-            const uploadedFile = localStorage.getItem('uploadedFile');
-            if (uploadedFile) {
-                // Jika ada, perbarui pratinjau dengan file tersebut
-                document.getElementById('pdf-preview-frame').src = uploadedFile;
-                // Tampilkan link preview PDF
-                document.getElementById('pdf-preview-link').style.display = 'block';
-                document.getElementById('preview-link').href = uploadedFile;
-            }
-        };
-
-        // Panggil fungsi updatePdfPreview saat dokumen diunggah
-        document.getElementById('formUpload').addEventListener('submit', function () {
-            updatePdfPreview();
-        });
     </script>
+    
 </body>
 
 </html>
